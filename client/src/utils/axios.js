61eaaa530413fbe3a50f31538cb0fdb97730982a
igmrrf <francis.igbiriki.fi@gmail.com>
2020-10-24 23:28:19 +0100

    Updating Environmental Variables and Scripts

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL:
-    process.env.REACT_APP_QWERTY_QUIZ_BASE_URL || 'http://localhost:3333/',
+  baseURL: process.env.BASE_URL || 'http://localhost:3333/',
 });
